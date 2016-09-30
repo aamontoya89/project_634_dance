3ac957282305704e0757141e4ffa21e0445e336d
aaron <aamontoya@gmail.com>
2016-09-29 20:04:25 -0400

    fix a little bit the trail, still not great

--- code/current/donald/sketch.js
+++ code/current/donald/sketch.js
@@ -40,7 +40,9 @@ var previousMoment = 0;
 //arrays for holding the trail information
 var trailX = [];
 var trailY = [];
+var trailMoment = [];
 var trailMaximumLength = 1000;
+var trailLifetime = 5000;
 
 //function for loading the assets
 function preload() {
@@ -326,8 +328,10 @@ function updateTrail() {
   if (mouseIsPressed) {
     //append to the end of the trails the current
     //mouse position
+    //also record the moment when they were created
     trailX.push(mouseX);
     trailY.push(mouseY);
+    trailMoment.push(millis());
 
     //if the length of the trails is longer
     //than the maximum, pop the first elements
@@ -338,8 +342,19 @@ function updateTrail() {
       trailY.splice(0, 1);
     }
 
+
+
   }
 
+  //check if they are still alive
+  //if the time since creation is larger than lifetime
+  //delete the particle
+  for (var i = 0; i < trailX.length; i++) {
+    if (millis() - trailMoment[i] > trailLifetime) {
+      trailX.splice(0, 1);
+      trailY.splice(0, 1);
+    }
+  }
 }
 
 //function for displaying the trail
@@ -349,7 +364,8 @@ function displayTrail() {
     for (var i = 0; i < trailX.length; i++) {
       for (var j = 0; j < trailY.length; j++) {
         fill(random(255), random(255), random(255));
-        var radius = 200*(trailX.length-i)/trailX.length
+        var radius = 50*(trailLifetime - (millis() - trailMoment[i]))/1000;
+        console.log(radius);
         ellipse(trailX[i], trailY[j], radius, radius);
       }
     }
