172ecb6ea1d8b0893e919c0087a427072001cf6e
yulicai <caiyuli127@gmail.com>
2016-10-23 12:56:04 -0400

    Modify ownKinectron trying to add threshold

--- code/rehearsals/rehearsal_2016_10_16/kinectron/js/ownKinectronClient.js
+++ code/rehearsals/rehearsal_2016_10_16/kinectron/js/ownKinectronClient.js
@@ -3,6 +3,12 @@ var kinectronCanvas = window.document.getElementById("kinectronCanvas");
 //retrieve the context of the canvas
 var context = kinectronCanvas.getContext("2d");
 
+//retrieve canvas on html document by id
+var kinectronCanvas2 = window.document.getElementById("kinectronCanvas2");
+//retrieve the context of the canvas
+var context2 = kinectronCanvas2.getContext("2d");
+
+var canvasData;
 //define the variables for kinectron constructor
 var username = "kinectron";
 var host = "172.16.246.240";
@@ -39,4 +45,27 @@ function setupKinectron() {
 //draw feed on the canvas
 function drawFeed(img) {
     context.drawImage(img, 10, 10);
+    canvasData = context.getImageData(0, 0, 500, 500);
+    for (var n = 0; n < 1000; n++) {
+            var x = Math.floor(Math.random() * canvasData.width);
+            var y = Math.floor(Math.random() * canvasData.height);
+            var pixIndex = y * canvasData.width + x;
+            var pixR = canvasData.data[pixIndex * 4 + 0];
+            var pixG = canvasData.data[pixIndex * 4 + 1];
+            var pixB = canvasData.data[pixIndex * 4 + 2];
+            // 0.21 R + 0.72 G + 0.07 B
+            var grayScale = 0.21*pixR + 0.72*pixG+0.07*pixB;
+            // if(grayScale>200){
+            ellipse(context,x,y,pixR,pixG,pixB,2);
+          // }
+          }
+}
+
+function ellipse(ctx,_x,_y,r,g,b,size){
+  ctx.save();
+  ctx.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
+  ctx.beginPath();
+  ctx.ellipse(_x, _y, size, size, 45 * Math.PI / 180, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.restore();
 }
